58c1429f1603bde26c58a65eb53595baad6ed6fd
Abhishek Kumar <abhishek.kumar@changecx.com>
2024-01-30 00:45:20 +0530

    configure relevance

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const algoliasearch = require("algoliasearch");
+const { result } = require("lodash");
 const client = algoliasearch("FW2J93JKNT", "b46512872d12ed8f55c3052c6cca08ad");
 const index = client.initIndex("T02_push");
-const lodash = require("lodash");
 
-const actors = require("./dummy.json");
+// const lodash = require("lodash");
+// const actors = require("./dummy.json");
+// const chunks = lodash.chunk(actors, 1);
+// chunks.forEach((chunk) => {
+//   index
+//     .saveObjects(chunk, {
+//       autoGenerateObjectIDIfNotExist: true,
+//     })
+//     .then(({ objectIDs }) => {
+//       console.log(objectIDs);
+//     });
+// });
 
-const chunks = lodash.chunk(actors, 1);
+// index.getSettings().then((result) => console.log(result));
+// index.setSettings({
+//   searchableAttributes: [
+//     "unordered(company)",
+//     "name",
+//     "title",
+//     "previous_titles",
+//   ],
+// });
 
-chunks.forEach((chunk) => {
-  index
-    .saveObjects(chunk, {
-      autoGenerateObjectIDIfNotExist: true,
-    })
-    .then(({ objectIDs }) => {
-      console.log(objectIDs);
-    });
+// index.setSettings({
+//   customRanking: ["desc(isFeatured)", "desc(endorsements)"],
+// });
+
+index.search("").then((result) => {
+  console.log(result.nbHits);
+
+  for (const hit of result.hits) {
+    console.log(hit);
+  }
 });
